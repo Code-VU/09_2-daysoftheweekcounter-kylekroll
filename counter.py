5fb5397dfd9733cb4beef0cd86198a37a44eff4e
Kyle Kroll <Kyle.Kroll@vu.com>
2023-04-02 15:57:39 -0500

    Completed assignment to specifications

--- counter.py
+++ counter.py
@@ -1,6 +1,19 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
+    handle = open(file_name)
+    days = list()
+    days_count = dict()
+    for line in handle:
+        if line.startswith('From'):
+            line_split = line.split()
+            if len(line_split) > 2:
+                days.append(line_split[2])
+            
+    for day in days:
+        days_count[day] = days_count.get(day, 0) + 1
+    
+    print(days_count)
 
 
 
